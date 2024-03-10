0ed467bec96dbf7a598109795597e981def821cc
Rokaya Nasr <rokanasr60@gmail.com>
2024-03-10 14:38:51 +0200

    datatypes

---
+++ dashboard/src/app/DataTypes/meals.ts
@@ -0,0 +1,18 @@
+export class Meal {
+  _id: string;
+  title: string;
+  category: string;
+  description: string;
+  price: string;
+  ingrediants: Array<string>;
+  imageFile: string
+  constructor(_id: string, title: string, category: string, description: string, price: string, ingrediants: Array<string>, imageFile: string) {
+      this._id = _id,
+      this.title = title,
+      this.category = category,
+      this.description = description,
+      this.price = price,
+      this.ingrediants = ingrediants,
+      this.imageFile = imageFile
+  }
+}
---
+++ dashboard/src/app/DataTypes/order.ts
@@ -0,0 +1,38 @@
+export class order {
+    _id: string;
+    userId: string;
+    customerId: string;
+    paymentIntentId: string;
+    meals: Array<MealI>;
+    subtotal: Number;
+    total: number;
+    shipping: shippingI;
+    delivery_status: string;
+    payment_status: string
+    constructor(_id: string, userId: string, customerId: string, paymentIntentId: string,
+        meals: Array<MealI>, subtotal: Number, total: number, shipping: shippingI,
+        delivery_status: string, payment_status:string) {
+            this._id = _id,
+            this.userId = userId,
+            this.customerId = customerId,
+            this.paymentIntentId = paymentIntentId,
+            this.meals = meals,
+            this.subtotal = subtotal,
+            this.total = total,
+            this.shipping = shipping,
+            this.delivery_status = delivery_status,
+            this.payment_status = payment_status
+    }
+}
+export interface MealI {
+    _id: string;
+    title: string;
+    price: string;
+    cartQuantity: number;
+}
+//crocky shipping interface
+export interface shippingI {
+    address: string,
+    city: string,
+    country: string,
+}
